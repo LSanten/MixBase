1e10c1e3bba69da8d41f28f610527112ea44b1f0
LSanten <leon.santen@icloud.com>
2019-01-12 12:28:26 -0500

    Organized databse management in classes Critical points: create global variables in function

--- MixBase.py
+++ MixBase.py
@@ -1,12 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('transition_database.db') #is being created automatically if doesn't exist - otherwise uses existing one
-c = conn.cursor()
+class DatabaseManagement():
+    """Class for database management, initialization etc"""
+    def __init__(self):
+        pass
 
-def create_table():
-    """Create table calles 'transitionTable' in database calles 'transition_database.db'"""
-    c.execute('CREATE TABLE IF NOT EXISTS transitionTable(id INTEGER primary key autoincrement, song_a TEXT not null, song_b TEXT not null, info TEXT not null)')
+    def connect_to_database_and_cursor(self, database_name):
+        """Connect to database and creates new database if not existent"""
+        global conn
+        conn = sqlite3.connect('transition_database.db') #is being created automatically if doesn't exist - otherwise uses existing one # TODO change database name to variable based on function input
+        global c
+        c = conn.cursor()
 
+    def create_table(self, transitionTable_name):
+        """Create table called 'transitionTable' in database calles 'transition_database.db'"""
+        c.execute('CREATE TABLE IF NOT EXISTS transitionTable(id INTEGER primary key autoincrement, song_a TEXT not null, song_b TEXT not null, info TEXT not null)')
+        #TODO change table name to variable based on name input from function
+
+    def close_connection(self):
+        c.close()
+        conn.close()
 
 class InputTransition():
     """Input from user - Song A - Song B - Information about transition"""
@@ -25,13 +38,15 @@ class InputTransition():
         print(self.transition_information)
 
     def data_entry(self):
-        c.execute("INSERT INTO transitionTable(song_a, song_b, info) VALUES('TEST A', 'SELF B', 'INFORMATION')")
+        """Saves  transition data in table """
+        c.execute("INSERT INTO transitionTable(song_a, song_b, info) VALUES(?, ?, ?)", (self.song_a_name, self.song_b_name, self.transition_information))
         conn.commit()
-        c.close()
-        conn.close()
+
 
 def Main():
-    create_table()
+    database = DatabaseManagement()
+    database.connect_to_database_and_cursor('transition_database.db')
+    database.create_table('transitionTable')
     while True:
         input_transition = InputTransition()
         input_transition.input_request() #request information about songs and save as attributes of class object
@@ -41,5 +56,7 @@ def Main():
             break
 
         input_transition.data_entry()
+
+    database.close_connection()
 if __name__ == '__main__':
     Main()
