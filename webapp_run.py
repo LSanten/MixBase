d1711978c366f9066d660125424622249a956150
LSanten <leon.santen@icloud.com>
2019-07-16 17:00:03 -0400

    use template inheritance to create html code

--- webapp_run.py
+++ webapp_run.py
@@ -12,19 +12,20 @@ from flask import render_template # to render HTML
 app = Flask(__name__)
 
 @app.route('/') #'/' tells us that it's the index of a page | access via  http://127.0.0.1:5000/
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('index.html', title="Home")
 
 
 
 #DEBUG Routes
-@app.route('/hello') # access via  http://127.0.0.1:5000/hello/anything
+"""@app.route('/hello') # access via  http://127.0.0.1:5000/hello/anything
 def hello():
-    return render_template('hello.html')
+    return render_template('hello.html')"""
 
 @app.route('/hello/<name>') # access via  http://127.0.0.1:5000/hello/*anything*
 def helloTemplate(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', name=name, title="Debug Route")
 
 if __name__ == '__main__':
     app.run(debug=True)
+    webbrowser.open_new_tab("http://127.0.0.1:5000/")
