52e2c5ec83d4bd1f48acd06022909db9195a6ad8
LSanten <leon.santen@icloud.com>
2019-07-23 18:48:02 -0400

    login and register work todo: redirect to home when logged in and click on register/login

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, flash, redirect
-from datetime import datetime
 from webapp import app, db, bcrypt
 from webapp.forms import RegistrationForm, LoginForm
 from webapp.models import User, Pair
+from flask_login import login_user, current_user
 
 
 @app.route('/') #'/' tells us that it's the index of a page | access via  http://127.0.0.1:5000/
@@ -11,6 +11,8 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # hashes entered password
@@ -23,13 +25,16 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@mixbase.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first() #database will be filtered with entered email
+        if user and bcrypt.check_password_hash(user.password, form.password.data): #check password and email
+            login_user(user, remember=form.remember.data) #login_user() logs user in
             return redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check username and password', 'danger') #danger makes return red #TODO: get success and danger colored frames working
+            flash('Login unsuccessful. Please check email and password', 'danger') #danger makes return red #TODO: get success and danger colored frames working
     return render_template('login.html', title='Login', form=form)
 
 #DEBUG Routes
