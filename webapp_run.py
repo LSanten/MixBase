b009f0c690a6131030238a4456fdcf830fdad9b5
LSanten <leon.santen@icloud.com>
2019-07-23 15:53:01 -0400

    add user model with backreference to pair model

--- webapp_run.py
+++ webapp_run.py
@@ -14,8 +14,19 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6fd5dd298e9002621b7e3c76bbf86372' #to get a random key: >>> python, >>> import secrets, >>> secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webmix.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webmix.db' # create database named "webmix.db"
 db = SQLAlchemy(app) # create database instance
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    pair_posts = db.relationship('Pair', backref='author', lazy=True) #this is a releationship not a column -- backreference to Pair. You can find out author of a pair post by using pair.author #lazy=True --> sqlalchemy will load data directly when used
+
+    def __repr__(self):
+        return f"User('{self.username}')"
 
 class Pair(db.Model): #data table for transition pair
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +36,7 @@ class Pair(db.Model): #data table for transition pair
     secondtartist = db.Column(db.String(30), nullable=False)
     comment = db.Column(db.Text)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Pair('{self.firstname}', '{self.secondname}', '{self.date_posted}' )"
